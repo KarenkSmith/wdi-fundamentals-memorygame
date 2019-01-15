f12e4db972c5553db678c8c7bfd7169c0f73ba8b
Karen Smith <karensmith@Karens-Air.local>
2019-01-15 14:32:48 -0800

    game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,21 +2,57 @@
 
 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+}
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",                 
+}
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+}
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png",
+}
+];
 var cardsinPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsinPlay.push(cardOne);
-cardsinPlay.push(cardTwo);
 
+var flipCard = function(cardId){
+	cardsinPlay.push(cards[cardId].rank);
+	console.log("user flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	
 if (cardsinPlay.length === 2){
+		checkFormatch();
+	}
+};
+
+var checkForMatch = function() {
 	if (cardsinPlay[0] === cardsinPlay[1]){
 		alert("you found a match!");
 	} else {
 		alert("sorry try again");
 	}
-}
+};
 
-console.log("user flipped " + cardsinPlay);
+
+flipCard(0);
+flipCard(2);
+	
+	
+	
+
+
+
 
