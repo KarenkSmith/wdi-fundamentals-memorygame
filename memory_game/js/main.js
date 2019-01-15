c458aae5ce21e8b04aad5afb9acbdc17acacfb2f
Karen Smith <karensmith@Karens-Air.local>
2019-01-15 07:49:17 -0800

    game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,22 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("user flipped " + cardTwo);
-console.log("user flipped " + cardThree);
+
+
+
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsinPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsinPlay.push(cardOne);
+cardsinPlay.push(cardTwo);
+
+if (cardsinPlay.length === 2){
+	if (cardsinPlay[0] === cardsinPlay[1]){
+		alert("you found a match!");
+	} else {
+		alert("sorry try again");
+	}
+}
+
+console.log("user flipped " + cardsinPlay);
+
